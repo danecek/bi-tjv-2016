4e3dc552b956cb28b29fd6c520170d437a24cf9d
danecek <danecek@fit.cvut.cz>
2017-01-06 19:37:39 +0100

    Merge origin/master
    Conflicts:
    	.gitignore

--- ClientRestUsers/src/clientrest/FXMain.java
+++ ClientRestUsers/src/clientrest/FXMain.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clientrest;
 
 import javafx.application.Application;
@@ -14,14 +9,11 @@ public class FXMain extends Application {
     @Override
     public void start(Stage primaryStage) {
         Scene scene = new Scene(new UsersPanel(), 300, 250);
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Users");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
